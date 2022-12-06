b2b7c9709b04ea12ce1183ef3da45225be722328
Ankit <112419242+ankit999221@users.noreply.github.com>
2022-12-06 23:58:35 +0530

    added marketcap

--- src/components/Content.js
+++ src/components/Content.js
@@ -4,7 +4,7 @@ function Content() {
   return (
     <>
       <div className=' flex w-full h-screen items-center justify-center sm:p-8 sm:pt-24 md:p-16 2xl:px-32 md:pt-32'>
-        <div className='rounded-2xl h-full w-full bg-[#fafbff] m-8'></div>
+        <div className='rounded-2xl h-full w-full bg-[#fafbff] '></div>
       </div>
     </>
   )
