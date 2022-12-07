b72ec79acd7adf46cbea09f0fa650c4221991257
Vaibhavi Kale <vaibhavikale482@gmail.com>
2022-12-07 14:28:14 +0530

    portfolio change

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,20 +1,17 @@
 import React from "react";
-import Content from "./components/Content"
+import Content from "./components/Content";
 import Navbar from "./components/Navbar";
 import Portfolio from "./components/Portfolio";
 
 function Dashboard() {
   return (
     <>
-<<<<<<< HEAD
       <Navbar />
-      <Home />
-      <Portfolio />
-=======
-      <Navbar/>
-      <Content/>
 
->>>>>>> 5ede0e994d111d8354ed4531537ec000d6b2012f
+      <Portfolio />
+
+      <Navbar />
+      <Content />
     </>
   );
 }
