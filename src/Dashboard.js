4748f4bbe576dea680ff20473c9335c2c51522d0
Vaibhavi Kale <vaibhavikale482@gmail.com>
2022-12-08 19:55:58 +0530

    dashboard commit

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,20 +1,13 @@
 import React from "react";
-import MarketCap from "./components/MarketCap";
 import Navbar from "./components/Navbar";
-import Portfolio from "./components/Portfolio";
-import Exchange from "./components/Exchange";
-import "./Dashboard.css"
-
+import "./Dashboard.css";
+import Content from "./components/Content";
 
 function Dashboard() {
   return (
     <>
-
-      <Navbar/>
-      <MarketCap/>
-      <Portfolio/>
-      <Exchange/>
-
+      <Navbar />
+      <Content />
     </>
   );
 }
