0853069796c25d5ddfc729844b1e7def033fe183
Satyam Kumar <112563832+satyamkumarsk@users.noreply.github.com>
2022-12-10 21:08:30 +0530

    edit

--- src/components/Exchange/Exchange.jsx
+++ src/components/Exchange/Exchange.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import React, {useState} from "react";
 import "./Exchange.css";
 
+
+
 function Exchange() {
+const [change , setChange]=useState("")
+
+const hendleClick=()=>{
+
+}
+
+const hendleBuyChange=(e)=>{
+    setChange(e.target.value)
+
+}
+
+const hendleShellChange=()=>{
+    
+
+}
   return (
     <>
       <div className="flex flex-col h-full bg-white w-full rounded-md p-6 gap-2 justify-evenly min-w-fit">
@@ -18,7 +35,7 @@ function Exchange() {
         </div>
         <div className="flex flex-col">
           <div className="flex flex-row">
-            <h4 className="text-orange-600 font-semibold text-xl h-fit my-auto w-[25%] justify-center pl-[10%]">
+            <h4 className="text-orange-600 font-semibold text-xl h-fit my-auto w-[25%] justify-center pl-[10%]" value={change} onChange={hendleShellChange}>
               Sell
             </h4>
             <div className="flex w-[30%]">
@@ -42,7 +59,7 @@ function Exchange() {
         </div>
         <div className="flex flex-col">
           <div className="flex flex-row">
-            <h4 className="text-green-600 font-semibold text-xl h-fit my-auto w-[23%] justify-center pl-[10%]">
+            <h4 className="text-green-600 font-semibold text-xl h-fit my-auto w-[22.5%] justify-center pl-[10%]" value={change} onChange={hendleBuyChange}>
               Buy
             </h4>
             <div className="flex w-[30%]">
@@ -62,7 +79,7 @@ function Exchange() {
           </div>
         </div>
         <div className="flex justify-center h-full">
-          <button className="hover:scale-105 duration-300 bg-gradient-to-tr from-cyan-400 to-blue-600 via-cyan-500 rounded-lg text-lg font-bold h-12 w-32">
+          <button className="hover:scale-105 duration-300 bg-gradient-to-tr from-cyan-400 to-blue-600 via-cyan-500 rounded-lg text-lg font-bold h-12 w-32" onClick={hendleClick}>
             Exchange
           </button>
         </div>
