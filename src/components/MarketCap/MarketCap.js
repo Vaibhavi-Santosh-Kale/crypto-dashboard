c75270f69957b85a90068b04e52a60ba61b85a13
Ankit <ankit999221@gmail.com>
2022-12-14 03:09:08 +0530

    added price and & info space on hover and click

--- src/components/MarketCap/MarketCap.js
+++ src/components/MarketCap/MarketCap.js
@@ -2,6 +2,7 @@ import "./MarketCap.css"
 import { VscTriangleUp, VscTriangleDown } from "react-icons/vsc"
 import { useSelector, useDispatch } from 'react-redux'
 import Loading from "../../assets/svg/loading.gif"
+import LoadingWhite from "../../assets/svg/loadingwhite.gif"
 import axios from "axios"
 import updatecap from "../state/actions/updatecap"
 import { useEffect} from "react"
@@ -12,6 +13,7 @@ function MarketCap() {
     const dispatch = useDispatch();
     const isLoading = useSelector((store) => store.marketloadingreducer);
     const curr = useSelector((store) => store.updatecurr);
+    const isDark = useSelector((store)=>store.themereducer);
 
     useEffect(() => {
         dispatch(marketloadchange(true));
@@ -38,18 +40,18 @@ function MarketCap() {
     //     fetchmarketdata();
     // }, 10000);
     return (
-        <div className='select-none rounded-xl h-full min-w-fit w-full flex-row-2 bg-white shadow-sm hover:cursor-default overflow-hidden' >
+        <div className={`select-none rounded-xl h-full min-w-fit w-full flex-row-2 ${isDark?"bg-black text-white":"bg-white"}  shadow-sm hover:cursor-default overflow-hidden`} >
             <div className='flex justify-center h-[45px]  shadow-sm border-b-2 text-2xl p-1'>
-                <span>{"Cryptocurrency Market Cap"}</span>
+                <span>Cryptocurrency Market Cap</span>
             </div>
             <ul className='h-[calc(100%-45px)] overflow-y-scroll'>
                 {
-                    isLoading === true ? <div className=" h-full w-full flex justify-center items-center">
+                    isLoading ? <div className=" h-full w-full flex justify-center items-center">
                         <div className="animate-pulse">
-                            <img className="h-16 w-16" src={Loading} alt="Loading..." />
+                            { isDark?<img className="h-16 w-16" src={Loading} alt="Loading..." />:<img className="h-16 w-16" src={LoadingWhite} alt="Loading..." />}
                         </div>
-                    </div> : store.map(({ id, image, market_cap, name, price_change_percentage_24h }) => (
-                        <span key={id} className='flex p-2 border-b-2 justify-between hover:scale-105 duration-200 hover:bg-gradient-to-r hover:from-cyan-50 hover:to-blue-300 hover:via-blue-200 hover:'>
+                    </div> : store.map(({ id, image, market_cap, name, price_change_percentage_24h, current_price}) => (     
+                        <span key={id} className='flex p-2 border-b-2 justify-between hover:scale-105 duration-200 hover:bg-gradient-to-r h-fit hover:from-cyan-50 hover:to-blue-300 hover:via-blue-200 hover:h-40' onClick={(event)=>{event.currentTarget.classList.toggle("h-40")}}>
 
                             <span className='flex font-bold text-1xl text-gray-700 w-fit'>
                                 <img className='sm:h-8 sm:w-8 h-10 w-10 m-1 rounded-full' src={image} alt="Currency Logo" />
@@ -58,17 +60,20 @@ function MarketCap() {
                                         {name}
                                     </span>
                                     <span className='text-[13px] text-gray-500'>
-                                        Mkt Cap :{(market_cap).toLocaleString('en-IN', { style: 'currency', currency: curr })}
+                                        Mkt Cap :{(market_cap)}
                                     </span>
                                 </div>
                             </span>
-                            <span className='flex my-auto w-20 mr-2 text-right'>
+                            <span className='flex flex-col w-fit mr-2 text-right'>
+                                <span className="text-gray-700 font-bold text-sm">Price: {(current_price).toLocaleString('en-IN', { style: 'currency', currency: curr })}</span>
+                                <div className="flex flex-row justify-end">
                                 <span className='flex m-1  '>
                                     {price_change_percentage_24h < 0 ? <VscTriangleDown color='red' size={20} /> : <VscTriangleUp color='green' size={20} />
                                     }
                                 </span>
-                                <span className='h-fit w-10'>{Math.abs(price_change_percentage_24h).toFixed(3)}</span>
-                                <span>{" %"}</span>
+                                <span className='h-fit w-10 text-gray-500'>{Math.abs(price_change_percentage_24h).toFixed(2)}</span>
+                                <span className="text-gray-500">{" %"}</span>
+                                </div>
                             </span>
                         </span>
                     ))}
