0d89b7d604deadf7c6f08930f146380779c2759c
Vaibhavi Kale <vaibhavikale482@gmail.com>
2022-12-24 10:40:30 +0530

    commit

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,10 +1,37 @@
 import React from "react";
+import { useSelector } from "react-redux";
+// import React, { useState, useEffect } from "react";
 import Chart from "react-apexcharts";
 import "./Portfolio.css";
 
 const Portfolio = () => {
+  const isDark = useSelector((state) => state.themereducer);
+  // const [coinName, setCoinName] = useState([]);
+  // const [coinNumber, setCoinNumber] = useState([]);
+
+  // useEffect(() => {
+  //   const coinName = [];
+  //   const coinNumber = [];
+  //   const getCoindata = async () => {
+  //     const reqData = await fetch("");
+  //     const resData = await reqData.json();
+  //     for (let i = 0; i < resData.length; i++) {
+  //       coinName.push(resData[i].name);
+  //       coinNumber.push(parseInt(resData[i].number));
+  //     }
+  //     setCoinName(coinName);
+  //     setCoinNumber(coinNumber);
+  //     //console.log(resData);
+  //   };
+
+  //   getCoindata();
+  // }, []);
   return (
-    <div className="bg-white ">
+    <div
+      className={`flex flex-col h-full w-full rounded-lg ${
+        isDark ? "bg-black text-white" : "bg-white text-black"
+      } p-4 gap-2`}
+    >
       <div className="flex justify-between">
         <h1 className="text-xl font-bold">Portfolio</h1>
         <div className="flex items-center">
