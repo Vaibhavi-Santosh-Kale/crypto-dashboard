4a46222a59d87b6495efed5bfc098ff594100e16
Vaibhavi Kale <vaibhavikale482@gmail.com>
2022-12-24 10:23:24 +0530

    commit

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -4,7 +4,14 @@ import "./Portfolio.css";
 
 const Portfolio = () => {
   return (
-    <div>
+    <div className="bg-white ">
+      <div className="flex justify-between">
+        <h1 className="text-xl font-bold">Portfolio</h1>
+        <div className="flex items-center">
+          <h4 className="text-sm font-light">Total Value</h4>
+          <span className="text-xl font-bold">$1000</span>
+        </div>
+      </div>
       <Chart
         type="pie"
         width={400}
