30117f49e7322760c36289df7deb472cb51a6986
Tarun <tarunjoshi757944@gmail.com>
2023-03-07 12:09:01 +0530

    colour

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -12,7 +12,7 @@ function Search() {
     return (
         <div className={`w-full flex rounded-lg ${isDark?"bg-black":"bg-white"}  shadow-sm p-2`}>
             <div className='flex m-auto mx-3 w-fit h-fit'>
-                <BsSearch size={19} color="TEAL BLUE"/>
+                <BsSearch size={19} color="gray"/>
             </div>
             <input value={sdata} className={`w-full ${isDark?"bg-black text-white":"bg-white"}`} onChange={(event)=>{dispatch(search(event.target.value));console.log(sdata)}} type="text" placeholder='Search' />
         </div>
