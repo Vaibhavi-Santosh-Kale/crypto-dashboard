fa8767aefa8b9d8ad4dbe23be5c395cc42e0a8f3
Ankit <112419242+ankit999221@users.noreply.github.com>
2023-03-20 22:28:30 +0530

    updated chart

--- src/components/Chart/ChartComponent.jsx
+++ src/components/Chart/ChartComponent.jsx
@@ -10,34 +10,45 @@ import { Bar } from "react-chartjs-2";
 
 // ChartJS.register(...registerables);
 
-  
+
 
 
 
 function ChartComponent() {
   const isDark = useSelector((state) => state.themereducer);
-  const coinList = useSelector((state)=>state.marketCap)
-  const [historicData, setHistoricData] = useState();
+  const coinList = useSelector((state) => state.marketCap);
+  const [historicData, setHistoricData] = useState([]);
+  const [chartSelector, setChartSelector] = useState("line");
   const [days, setDays] = useState(1);
+  const currencySelector = useSelector((state) => state.updatecurr);
+  const [cointofetch, setCoinToFetch] = useState("Bitcoin");
+  console.log(cointofetch);
+
+
   // const { currency } = CryptoState();
 
   const fetchHistoricData = async () => {
-    await axios.get(`https://api.coingecko.com/api/v3/coins/bitcoin/market_chart?vs_currency=inr&days=30`).then((res)=>{setHistoricData(res.data.prices);
-    console.log(historicData)});
-    
+    await axios.get(`https://api.coingecko.com/api/v3/coins/${cointofetch}/market_chart?vs_currency=${currencySelector}&days=${days}`).then((res) => {
+      setHistoricData(res.data.prices);
+      console.log(historicData)
+    }).catch((error) => {
+      setTimeout(() => {
+        fetchHistoricData();
+      }, 20000);
+      console.log(error, "api request limit exceed");
+    });
   };
 
   useEffect(() => {
     fetchHistoricData();
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [cointofetch, currencySelector, days]);
 
 
   return (
     <div
-      className={`flex flex-col h-full w-full rounded-lg ${
-        isDark ? "bg-black text-white" : "bg-white text-black"
-      } p-4 gap-2`}
+      className={`flex flex-col h-full w-full rounded-lg ${isDark ? "bg-black text-white" : "bg-white text-black"
+        } p-4 gap-2`}
     >
       <div className="h-12 flex flex-row items-center justify-center gap-5">
         <div className="h-full w-full flex flex-row gap-3 justify-end items-center">
@@ -49,88 +60,91 @@ function ChartComponent() {
           <Button value={<MdDateRange size={24} color="slate" />} />
         </div>
         <div className="h-full w-full flex flex-row items-center justify-around">
-          <select className="h-full outline outline-1 outline-gray-500 rounded-lg flex items-center justify-center">
-            {coinList.map(({id,name})=>(
+          <select className="h-full outline outline-1 outline-gray-500 rounded-lg flex items-center justify-center" onChange={(event) => { setCoinToFetch(event.target.value) }}>
+            {coinList.map(({ id, name }) => (
               <option key={id} value={name}>  {name}</option>
             ))}
           </select>
-          <select className="h-full outline outline-1 outline-gray-500 rounded-lg flex items-center justify-center">
-            <option selected value="Line">Line</option>
-            <option value="Bar">Bar</option>
+          <select className="h-full outline outline-1 outline-gray-500 rounded-lg flex items-center justify-center" onChange={(event) => {
+            setChartSelector(event.target.value); console.log(chartSelector)
+          }}>
+            <option selected value="line">Line</option>
+            <option value="bar">Bar</option>
 
           </select>
         </div>
       </div>
-      <div className="h-full">
+      <div className="h-[calc(100%-55px)]">
         {/* Yes i am a chart component its still on deveploment */}
-        
 
-        <Line
-              data={{
-                labels: historicData.map((coin) => {
-                  let date = new Date(coin[0]);
-                  let time =
-                    date.getHours() > 12
-                      ? `${date.getHours() - 12}:${date.getMinutes()} PM`
-                      : `${date.getHours()}:${date.getMinutes()} AM`;
-                  return days === 1 ? time : date.toLocaleDateString();
-                }),
 
-                datasets: [
-                  {
-                    data: historicData.map((coin) => coin[1]),
-                    label: `Price ( Past ${days} Days ) in`,
-                    borderColor: "#EEBC1D",
-                  },
-                ],
-              }}
-              options={{
-                
-                  maintainAspectRatio: false,
-              
-          
-                elements: {
-                  point: {
-                    radius: 1,
-                  },
+        {chartSelector === "line" ? <Line
+          data={{
+            labels: historicData.map((coin) => {
+              let date = new Date(coin[0]);
+              let time =
+                date.getHours() > 12
+                  ? `${date.getHours() - 12}:${date.getMinutes()} PM`
+                  : `${date.getHours()}:${date.getMinutes()} AM`;
+              return days === 1 ? time : date.toLocaleDateString();
+            }),
+
+            datasets: [
+              {
+                data: historicData.map((coin) => coin[1]),
+                label: `Price ( Past ${days} Days ) in `,
+                borderColor: "#1DEE2A",
+              },
+            ],
+          }}
+          options={{
+
+            maintainAspectRatio: false,
+
+
+            elements: {
+              point: {
+                radius: 1,
+              },
+            },
+          }}
+        />
+          : <Bar
+            data={{
+              labels: historicData.map((coin) => {
+                let date = new Date(coin[0]);
+                let time =
+                  date.getHours() > 12
+                    ? `${date.getHours() - 12}:${date.getMinutes()} PM`
+                    : `${date.getHours()}:${date.getMinutes()} AM`;
+                return days === 1 ? time : date.toLocaleDateString();
+              }),
+
+              datasets: [
+                {
+                  data: historicData.map((coin) => coin[1]),
+                  label: `Price ( Past ${days} Days ) in `,
+                  fillColor: ["rgba(0,10,220,0.5)", "rgba(220,0,10,0.5)", "rgba(220,0,0,0.5)", "rgba(120,250,120,0.5)"],
+                  strokeColor: "rgba(220,220,220,0.8)",
+                  highlightFill: "rgba(220,220,220,0.75)",
+                  highlightStroke: "rgba(220,220,220,1)",
                 },
-              }}
-            />
-        {/* <Bar 
-              data={{
-                labels: "abc",
+              ],
+            }}
+            options={{
 
-                datasets: [
-                  {
-                    data: 1,
-                    label: "test",
-                    borderColor: "#EEBC1D",
-                  },
-                ],
-              }}
-              options={{
-                elements: {
-                  point: {
-                    radius: 1,
-                  },
+              maintainAspectRatio: false,
+
+
+              elements: {
+                point: {
+                  radius: 1,
                 },
-              }}
-            /> */}
-        
-            
-              {/* {chartDays.map((day) => (
-                <SelectButton
-                  key={day.value}
-                  onClick={() => {setDays(day.value);
-                    setflag(false);
-                  }}
-                  selected={day.value === days}
-                >
-                  {day.label}
-                </SelectButton>
-              ))} */}
-            
-        
+              },
+            }}
+          />}
+
+
       </div>
     </div>
   );
