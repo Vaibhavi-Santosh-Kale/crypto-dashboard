ec4840b22a5e5aa4b5aa289dd8bff837a38b9d90
Satyam Kumar <112563832+satyamkumarsk@users.noreply.github.com>
2023-03-22 21:04:22 +0530

    action

--- src/redux/action.js
+++ src/redux/action.js
@@ -1,30 +1,6 @@
 import axios from "axios";
 import actionTypes from "./types";
 
-// fetchCoins action
-export const fetchCoins = () => {
-  return (dispatch) => {
-    axios
-      .get(
-        `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&page=1&per_page=9`
-      )
-      .then((response) => {
-        const data = response.data;
-        dispatch({
-          type: actionTypes.COIN_API_SUCCESS,
-          payload: data,
-        });
-      })
-      .catch((error) => {
-        const errorMsg = error.message;
-        dispatch({
-          type: actionTypes.COIN_API_ERROR,
-          payload: errorMsg,
-        });
-      });
-  };
-};
-
 // exchange rate action
 export const fetchCoinList = () => {
   return (dispatch) => {
