1e4ddb87624ce6c70e9f82d7fc099a1cbfc275ad
Satyam Kumar <112563832+satyamkumarsk@users.noreply.github.com>
2023-03-22 08:42:14 +0530

    change

--- src/components/state/reducers/exchangeReducer.js
+++ src/components/state/reducers/exchangeReducer.js
@@ -1,27 +1,25 @@
-import actionTypes from "../actions/types"
+import actionTypes from "../actions/types";
 
 const initialState = {
-    projectName:"Crypto Exchange",
-    coinList:  [],
+  projectName: "Crypto Exchange",
+  coinList: [],
+};
+
+function exchangeReducer(state = initialState, action) {
+  switch (action.type) {
+    case actionTypes.EXCHANGE_SUCCESS:
+      return {
+        ...state,
+        coinList: action.payload,
+      };
+
+    case actionTypes.EXCHANGE_ERROR:
+      alert(action.payload);
+      return state;
+
+    default:
+      return state;
+  }
 }
 
-function exchangeReducer (state=initialState, action) {
-
-    switch(action.type) {
-
-        case actionTypes.EXCHANGE_SUCCESS:
-            return {
-                ...state,
-                coinList : action.payload
-            }
-
-        case actionTypes.EXCHANGE_ERROR:
-            alert(action.payload)
-            return state
-
-        default: return state
-    }
-}
-
-export default exchangeReducer
-
+export default exchangeReducer;
